25f1c13f680665ead09780d7aa5f216c532f6984
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2025-03-25 23:45:38 +0300

    fix show more content

--- src/js/modal-callback.js
+++ src/js/modal-callback.js
@@ -1,20 +1,34 @@
 (() => {
+	// document.removeEventListener('keydown', onEscKeyPress);
 	const modalCallback = document.querySelector('.js-callback');
 	const openCallbackBtn = document.querySelector('.js-open-callback');
 	const openCallbackBtnMenu = document.querySelector('.js-open-callback-menu');
 	const closeCallbackBtn = document.querySelector('.js-close-callback');
 
-	const toggleMenu = () => {
+	const toggleCallback = () => {
+		// document.removeEventListener('keydown', onEscKeyPress);
 		const isMenuOpen =
 			openCallbackBtn.getAttribute('aria-expanded') === 'true' || false;
 		openCallbackBtn.setAttribute('aria-expanded', !isMenuOpen);
 		modalCallback.classList.toggle('is-hidden');
+		// document.addEventListener('keydown', onEscKeyPress);
 
 		const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
 		bodyScrollLock[scrollLockMethod](document.body);
 	};
 
-	openCallbackBtn.addEventListener('click', toggleMenu);
-	openCallbackBtnMenu.addEventListener('click', toggleMenu);
-	closeCallbackBtn.addEventListener('click', toggleMenu);
+	// function onEscKeyPress(event) {
+	// 	const ESC_KEY_CODE = 'Escape';
+	// 	const isEscKey = event.code === ESC_KEY_CODE;
+
+	// 	if (isEscKey) {
+	// 		toggleCallback();
+	// 	}
+
+	// 	document.removeEventListener('keydown', onEscKeyPress);
+	// }
+
+	openCallbackBtn.addEventListener('click', toggleCallback);
+	openCallbackBtnMenu.addEventListener('click', toggleCallback);
+	closeCallbackBtn.addEventListener('click', toggleCallback);
 })();
--- src/js/modal-feedback.js
+++ src/js/modal-feedback.js
@@ -1,20 +1,36 @@
 (() => {
+	// document.removeEventListener('keydown', onEscKeyPress);
 	const modalFeedback = document.querySelector('.js-feedback');
 	const openFeedbackBtn = document.querySelector('.js-open-feedback');
 	const openFeedbackBtnMenu = document.querySelector('.js-open-feedback-menu');
 	const closeFeedbackBtn = document.querySelector('.js-close-feedback');
+	// const closeFeedbackBlurArea = document.querySelector('.js-close-blurarea');
 
-	const toggleMenu = () => {
+	const toggleFeedback = () => {
+		// document.removeEventListener('keydown', onEscKeyPress);
 		const isMenuOpen =
 			openFeedbackBtn.getAttribute('aria-expanded') === 'true' || false;
 		openFeedbackBtn.setAttribute('aria-expanded', !isMenuOpen);
 		modalFeedback.classList.toggle('is-hidden');
+		// document.addEventListener('keydown', onEscKeyPress);
 
 		const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
 		bodyScrollLock[scrollLockMethod](document.body);
 	};
 
-	openFeedbackBtn.addEventListener('click', toggleMenu);
-	openFeedbackBtnMenu.addEventListener('click', toggleMenu);
-	closeFeedbackBtn.addEventListener('click', toggleMenu);
+	// function onEscKeyPress(event) {
+	// 	const ESC_KEY_CODE = 'Escape';
+	// 	const isEscKey = event.code === ESC_KEY_CODE;
+
+	// 	if (isEscKey) {
+	// 		toggleFeedback();
+	// 	}
+
+	// 	document.removeEventListener('keydown', onEscKeyPress);
+	// }
+
+	openFeedbackBtn.addEventListener('click', toggleFeedback);
+	openFeedbackBtnMenu.addEventListener('click', toggleFeedback);
+	closeFeedbackBtn.addEventListener('click', toggleFeedback);
+	// closeFeedbackBlurArea.addEventListener('click', toggleFeedback);
 })();
